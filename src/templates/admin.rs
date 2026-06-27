988e53941ef2b9714b7f5d2642ba893109409db3
csd113 <xxcsd113xx@gmail.com>
2026-06-26 18:27:15 -0700

    Update admin login button label

--- src/templates/admin.rs
+++ src/templates/admin.rs
@@ -49,7 +49,7 @@ pub fn admin_login_page(
   <input type="password" name="password" required autocomplete="current-password">
 </label>
 <div class="admin-login-actions">
-  <button type="submit">Login</button>
+  <button type="submit">authenticate</button>
 </div>
 </form>
 </div>"#,
